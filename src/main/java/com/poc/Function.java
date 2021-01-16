87d7bbe92ade36f15963986a99fe30b455fd5c91
Rahul07 <88mydestiny@gmail.com>
2021-01-16 12:26:16 +0530

    Azure functions hello world

---
+++ src/main/java/com/poc/Function.java
@@ -0,0 +1,45 @@
+package com.poc;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import com.poc.Person;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it
+     * using "curl" command in bash: 1. curl -d "HTTP Body" {your
+     * host}/api/HttpExample 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+    @FunctionName("HttpExample")
+    public HttpResponseMessage run(@HttpTrigger(name = "req", methods = { HttpMethod.GET,
+            HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameter
+        final String query = request.getQueryParameters().get("name");
+        final String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body("Please pass a name on the query string or in the request body").build();
+        } else {
+            Person person = new Person();
+            person.setName(name);
+            return request.createResponseBuilder(HttpStatus.OK)
+                    .body("Hello, " + person.getName() + " - " + person.getRandomDiscount()).build();
+        }
+    }
+}
---
+++ src/main/java/com/poc/Person.java
@@ -0,0 +1,20 @@
+package com.poc;
+
+public class Person {
+
+    private String name;
+    private String randomDiscount;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getRandomDiscount() {
+        return "10%";
+    }
+
+}
